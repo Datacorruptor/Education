e11be36876492dbd54da6fdcb2e63ec27ae322ae
IvanSmitt <kkkkck.vanya@yandex.ru>
2020-09-04 11:46:47 +0300

    1

---
+++ console1/Chapter01/Lesson4/Completed/Lesson4-exercise1-completed-cs/Converting/Converting/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Converting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int16 i16 = 1;
+            Int32 i32 = 1;
+            double db = 1;
+
+            i16 = i32;
+            i16 = db;
+
+            i32 = i16;
+            i32 = db;
+
+            db = i16;
+            db = i32;
+
+
+        }
+    }
+}
