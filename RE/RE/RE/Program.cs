8dc1cc87d7f51b000ca5a013e1d6aa6650ad2c88
IvanSmitt <kkkkck.vanya@yandex.ru>
2020-09-18 10:19:03 +0300

    1

---
+++ RE/RE/RE/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace RE
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] text ={"Макс",
+        "C++",
+        " +79263772622",
+        " 89263772622",
+        " (926)3772622" };
+
+      foreach (string item in text)
+      {
+        string pattern = @"[+]?[1-9]?\s?[(]?[0-9]{3}[)]?[\s-]?[0-9]{3}[\s-]?[0-9]{2}[\s-]?[0-9]{2}";
+        if (Regex.IsMatch(item, pattern))
+        {
+          //Console.WriteLine($"{item} это телефон"); 
+          Console.WriteLine(item + " это телефон");
+        }
+
+        else
+        {
+          Console.WriteLine(
+          string.Format("{0} это НЕ телефон", item)
+          );
+        }
+
+      }
+    }
+  }
+}
